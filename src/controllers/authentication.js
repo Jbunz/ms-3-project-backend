6f90bcf70724319a7351adf56aa21fa516f9a728
Dalton Tewanger <daltontewanger@yahoo.com>
2024-07-28 12:03:23 -0700

    Updated get route for searching devs as a recruiter

--- src/controllers/authentication.js
+++ src/controllers/authentication.js
@@ -71,8 +71,11 @@ router.post('/signin', async (req, res) => {
 });
 
 // Auth route
-router.get('/profile', async (req, res) => {
-  res.json(req.user)
-})
+router.get('/profile', (req, res) => {
+  if (!req.user) {
+    return res.status(204).send()
+  }
+  res.json(req.user);
+});
 
 module.exports = router;
--- src/controllers/recruiterController.js
+++ src/controllers/recruiterController.js
@@ -1,11 +1,15 @@
+const express = require('express');
+const router = express.Router();
 const User = require('../models/User');
 
-// Get all developers (for recruiters)
-exports.getAllDevelopers = async (req, res) => {
-    try {
-        const developers = await User.find({ role: 'developer' });
-        res.status(200).json(developers);
-    } catch (err) {
-        res.status(500).json({ error: 'Server error' });
-    }
-};
+// New route for fetching all developers
+router.get("/devs", async (req, res) => {
+  try {
+    const developers = await User.find({ role: "Developer" });
+    res.status(200).json(developers);
+  } catch (err) {
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+module.exports = router;
