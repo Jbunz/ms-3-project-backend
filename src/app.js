09d481243dedadd3d4fb37c688a4cb4b98df2b51
Dalton Tewanger <daltontewanger@yahoo.com>
2024-07-28 23:28:05 -0700

    changed dev schema to properly have an empty array of languages when added as a user, updated cors in app.js

--- src/app.js
+++ src/app.js
@@ -3,21 +3,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const defineCurrentUser = require('./middleware/defineCurrentUser');
-const path = require('path');
 const app = express();
 const connectDB = require('./database'); 
 
 // Express Settings
 app.use(cors({
-    origin: 'http://localhost:3000',
-    methods: 'GET,POST,PUT,DELETE',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'authToken']
   }));
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(defineCurrentUser);
-app.use('/public', express.static(path.join(__dirname, '..', 'public')));
 
 // Connect to MongoDB
 connectDB();
--- src/models/Developer.js
+++ src/models/Developer.js
@@ -4,7 +4,7 @@ const User = require('./User');
 const DeveloperSchema = new mongoose.Schema({
   profile: {
     description: { type: String, default: 'Your description goes here' },
-    languages: { type: [String], default: [''] },
+    languages: { type: [String], default: [] },
     projects: { type: String, default: 'Enter your projects here' },
     profileImage: { type: String, default: 'frog-profile.jpg' }
   }
