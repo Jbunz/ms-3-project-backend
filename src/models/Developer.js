a28874a0167199042472df61eb696df8825109ec
Courtney <crudd12@gmail.com>
2024-07-30 18:26:22 -0700

    Update Developer.js - updating default picture

--- src/models/Developer.js
+++ src/models/Developer.js
@@ -6,7 +6,7 @@ const DeveloperSchema = new mongoose.Schema({
     description: { type: String, default: 'Your description goes here' },
     languages: { type: [String], default: [] },
     projects: { type: String, default: 'Enter your projects here' },
-    profileImage: { type: String, default: 'frog-profile.jpg' }
+    profileImage: { type: String, default: 'https://www.shutterstock.com/image-vector/default-avatar-profile-icon-social-600nw-1677509740.jpg' }
   }
 });
 
